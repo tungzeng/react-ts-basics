50889fb0ecac9ec5f9b81063934fc0c3c5bbe1e3
Giang <giangvg395@gmail.com>
2024-03-27 16:15:07 +0700

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,41 @@
-import CourseGoal from "./components/CourseGoal.tsx";
+import CourseGoalList from "./components/CourseGoalList.tsx";
+import Header from "./components/Header.tsx";
+import goalsImg from "./assets/goals.jpg";
+import NewGoal from "./components/NewGoal.tsx";
+
+import { useState } from "react";
+
+export type CourseGoal = {
+  title: string;
+  description: string;
+  id: number;
+};
 
 export default function App() {
+  const [goals, setGoals] = useState<CourseGoal[]>([]);
+
+  function handleAddGoal(goal: string, summary: string) {
+    setGoals((prevGoals) => {
+      const newGoal: CourseGoal = {
+        id: Math.random(),
+        title: goal,
+        description: summary,
+      };
+      return [...prevGoals, newGoal];
+    });
+  }
+
+  function handleDeleteGoal(id: number) {
+    setGoals((prevGoals) => prevGoals.filter((goal) => goal.id !== id));
+  }
+
   return (
     <main>
-      <CourseGoal
-        title="Learn React + TS"
-        description="Learn it from the ground up"
-      />
+      <Header image={{ src: goalsImg, alt: "A list of goals" }}>
+        <h1>Your Course Goals</h1>
+      </Header>
+      <NewGoal onAddGoal={handleAddGoal}></NewGoal>
+      <CourseGoalList goals={goals} onDeleteGoal={handleDeleteGoal} />
     </main>
   );
 }
--- src/components/CourseGoal.tsx
+++ src/components/CourseGoal.tsx
@@ -1,16 +1,24 @@
-type CourseGoalProps = {
-  title: string;
-  description: string;
-};
+import { PropsWithChildren } from "react";
 
-export default function CourseGoal({ title, description }: CourseGoalProps) {
+type CourseGoalProps = PropsWithChildren<{
+  id: number;
+  title: string;
+  onDelete: (id: number) => void;
+}>;
+
+export default function CourseGoal({
+  id,
+  title,
+  children,
+  onDelete,
+}: CourseGoalProps) {
   return (
     <article>
       <div>
         <h2>{title}</h2>
-        <p>{description}</p>
+        {children}
       </div>
-      <button>Delete</button>
+      <button onClick={() => onDelete(id)}>Delete</button>
     </article>
   );
 }
